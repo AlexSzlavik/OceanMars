b0f10c94adfa795d71436c4bc6836382ddd5b764
Alex Szlavik <aszlavik@uwaterloo.ca>
2012-09-29 00:15:09 -0400

    Cleared a bunch of warnings

--- OceanMars/OceanMars/Client/GameStateManager/ScreenManager.cs
+++ OceanMars/OceanMars/Client/GameStateManager/ScreenManager.cs
@@ -1,12 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
-
-
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using Microsoft.Xna.Framework;
