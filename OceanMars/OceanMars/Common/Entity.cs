993589d49e03263922d1a580a0b804cd69085a75
Sherban <s@s-PC.(none)>
2012-09-28 23:51:41 -0400

    Added First Entity

---
+++ OceanMars/OceanMars/Common/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace OceanMars.Common
+{
+    public class Entity
+    {
+        int id;
+        Vector2 collisionBox;
+        Entity parent;
+        List<Entity> children;
+        Matrix transform;
+        Vector2 velocity;
+
+        Matrix getWorldTransform() { return parent.getWorldTransform() * transform; }
+    }
+}
