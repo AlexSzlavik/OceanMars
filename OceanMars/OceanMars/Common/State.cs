45ccdc48718f3a05e7e148b48bf330db567587ed
Fil Krynicki <filipkrynicki@gmail.com>
2012-09-29 01:41:25 -0400

    Added nextframe update

--- OceanMars/OceanMars/Common/State.cs
+++ OceanMars/OceanMars/Common/State.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace OceanMars.Common
 {
     public class State
     {
+        private const Vector2 GRAVITY = new Vector2(0, -9.8f);
+
         public World root;
         Dictionary<int, Entity> entities = new Dictionary<int,Entity>();
 
         void nextFrame()
         {
-            
+            foreach (Entity child in root.children)
+            {
+                if (child is EllipseEntity)
+                {
+                    EllipseEntity ellipseEntity = (EllipseEntity)child;
+                    ellipseEntity.velocity += GRAVITY;
+                    ellipseEntity.testCollision(entities.Values.ToList()); // TODO: using ToList may be inefficient
+                }
+                // Do not do collisions for SliderEntities
+            }
         }
     }
 }
