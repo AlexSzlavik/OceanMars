49da646e710431036a23f205c3f312158f9a669e
Sherban <s@s-PC.(none)>
2012-09-29 00:30:35 -0400

    committing to see if Alex's fix worked

--- OceanMars/OceanMars/Common/EllipseEntity.cs
+++ OceanMars/OceanMars/Common/EllipseEntity.cs
@@ -8,5 +8,20 @@ namespace OceanMars.Common
 {
     class EllipseEntity : Entity
     {
+        public Vector2 collisionEllipse { public get; private set; }
+
+        public EllipseEntity(Vector2 size)
+        {
+            collisionEllipse = size;
+        }
+
+        public void testCollision(Entity entity)
+        {
+            //Assumes that the entity manager checks AABBs to see if testing collisions makes sense
+
+            //Check if Entity is a SliderEntity
+
+            //Check if Entity is an EllipseEntity
+        }
     }
 }
