51bdd945ec59b1ba0b9c2f3ccbce6d212c4b608d
Fil Krynicki <filipkrynicki@gmail.com>
2012-09-29 12:18:46 -0400

    Test level for compiling with

---
+++ OceanMars/OceanMars/Common/TestLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace OceanMars.Common
+{
+    public class TestLevel : Entity
+    {
+        public TestLevel() : base (new Vector2(0, 0))
+        {
+        }
+    }
+}
